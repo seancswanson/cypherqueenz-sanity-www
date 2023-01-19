02d1a89bad7a3cd858dee6aaacdf0d589073eb3c
Sean Swanson <>
2023-01-18 23:33:06 -0800

    Adjusted production_www dataset

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'k34rqpdo',
-    dataset: 'production'
+    dataset: 'production_www'
   }
 })
